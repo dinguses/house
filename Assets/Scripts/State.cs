a6e3b66f4ca3cf86489192d7c54c8943396e6353
Kevin M Granger <github@kevinmgranger.me>
2016-07-29 21:29:58 -0400

    Better xml example, beginnings of some concretions and convenience methods.
    Current thoughts: Current extensions are too concrete. Consider allowing those to be LINQ-able too.

---
+++ Assets/Scripts/State.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections.Generic;
+using System.Xml.Linq;
+using System.Linq;
+
+public struct State
+{
+    public readonly XElement self;
+
+    public State(XElement self)
+    {
+        if (self == null) throw new System.Exception();
+
+        this.self = self;
+    }
+
+    public string Img
+    {
+        get
+        {
+            return self.AttributeOrElement("img").FirstOrDefault();
+        }
+    }
+
+    public string Name
+    {
+        get
+        {
+            return self.NodeNameOrAttrOrValue("state");
+        }
+    }
+
+    public string Description
+    {
+        get
+        {
+            return self.AttributeOrElement("desc", "description", "text").FirstOrDefault();
+        }
+    }
+
+    public static implicit operator XElement(State s)
+    {
+        return s.self;
+    }
+
+    public static implicit operator string(State s)
+    {
+        return s.Name;
+    }
+}
