cb3d53a9b5d61f9b88987dfe0ebf5d12a503f758
Kevin M Granger <github@kevinmgranger.me>
2016-07-31 18:02:26 -0400

    Add shell lexer, beginnings of true command system

---
+++ Assets/Scripts/Commander.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Commander : MonoBehaviour
+{
+    public Text text;
+
+    public void OnValidate()
+    {
+        if (text == null) text = GetComponent<Text>();
+    }
+
+    public void Start()
+    {
+      
+    }
+
+    public void Update()
+    {
+    }
+
+    public void Clear()
+    {
+        text.text = "";
+    }
+
+    public void Append(string txt)
+    {
+        this.text.text += "\n" + txt + "\n" ;
+    }
+
+    public void RunCommand(string command)
+    {
+        Debug.Log("Running command: ", this);
+
+        var tokens = command.Shlex();
+
+        var sb = new StringBuilder();
+        foreach (var item in tokens)
+        {
+            sb.Append(item);
+            sb.Append(", ");
+        }
+
+        Debug.LogFormat("cmd: {0}", sb.ToString());
+    }
+}
---
+++ Assets/ShLexer.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+
+public static class ShLexer
+{
+    static bool inquote;
+    static List<string> tokens = new List<string>();
+    static StringBuilder sb = new StringBuilder();
+
+    private static void Reset()
+    {
+        inquote = false;
+        tokens.Clear();
+        sb.Length = 0;
+    }
+
+    /// <summary>
+    /// Lex a string, handling whitespace and quotes properly.
+    /// 
+    /// Unbalanced quotes will fail out by returning null.
+    /// </summary>
+    /// <param name="self"></param>
+    /// <returns></returns>
+    public static List<string> Shlex(this string self)
+    {
+        Reset();
+        foreach (var chr in self)
+        {
+            if (inquote)
+            {
+                switch (chr)
+                {
+                    case '"':
+                        inquote = false;
+                        break;
+                    default:
+                        sb.Append(chr);
+                        break;
+                }
+            }
+            else
+            {
+                switch (chr)
+                {
+                    case '"':
+                        inquote = true;
+                        break;
+                    case ' ':
+                        if (sb.Length == 0) break;
+                        tokens.Add(sb.ToString());
+                        sb.Length = 0;
+                        break;
+                    default:
+                        sb.Append(chr);
+                        break;
+                }
+            }
+        }
+
+        if (inquote) return null;
+
+        if (sb.Length != 0) tokens.Add(sb.ToString());
+
+        return tokens;
+    }
+}
